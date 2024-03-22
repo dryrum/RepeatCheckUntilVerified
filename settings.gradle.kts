18a67ba5acd0b4f08f813101b5e8cb24ae9cc8d7
Carmelo Iriti <ciriti@gmail.com>
2024-03-22 15:12:21 +0100

    gitignore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "org.jetbrains.kotlin") {
+                useVersion("1.9.0")
+            }
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
